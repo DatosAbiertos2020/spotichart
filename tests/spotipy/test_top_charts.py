1033ceead0e258278148ece2c328f5e97b4abdb6
Manolo Perez <manolope338@gmail.com>
2019-10-15 22:48:17 -0500

    Update First attempt for tests

--- tests/spotipy/test_top_charts.py
+++ tests/spotipy/test_top_charts.py
@@ -13,7 +13,7 @@ class TestSum(unittest.TestCase):
         self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
 
     def test_sum_tuple(self):
-        self.assertEqual(sum((1, 2, 2)), 6, "Should be 6")
+        self.assertFalse(sum((1, 2, 2)) == 6, "Should be 6")
 
 
 if __name__ == '__main__':
